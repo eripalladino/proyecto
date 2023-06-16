663b9332c8409d24e39485d83d84fffeb47a1501
Erika Palladino <eripalladino@gmail.com>
2023-06-16 13:04:34 -0300

    cambioNavbar

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -7,28 +7,25 @@ import { CardActionArea } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import IconButton from '@mui/material/IconButton';
 
-const CardUser = ({char}) => {
+const CardUser = ({ char }) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image= {char.image} sx= {{height: 250, width: 200}}
-          alt="green iguana"
+          image={char.image} sx={{ height: 250, width: 200 }}
+          alt="image"
         />
         <CardContent>
           <Typography gutterBottom variant="h8" component="div">
             {char.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-          {char.category}
+            {char.category}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             ${char.price}
-            <IconButton color="primary" aria-label="add to shopping cart">
-            <AddShoppingCartIcon />
-          </IconButton>
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,18 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import "./NavBar.css"
 
-export default function ButtonAppBar() {
+const NavBar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <nav className='nav-container'>
+      <ul className='nav-ul'>
+        <li className='nav-link'>Inicio</li>
+        <li className='nav-link'>About</li>
+        <li className='nav-link'>Contact</li>
+        <li className='nav-link'> <ShoppingCartIcon/> <span>2</span> </li>
+      </ul>
+    </nav>
+  )
 }
+
+export default NavBar
